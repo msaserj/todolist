389c9ddf19ead632d4c61c1609fd78f67a3fe55d
msaserj <serg.ks@gmail.com>
2022-05-29 00:31:37 +0300

    01-monday. props for todolist title

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import {Todolist} from "./Todolist";
 function App() {
     return (
         <div className="App">
-            <Todolist />
-            <Todolist />
-            <Todolist />
+            <Todolist title="What to learn?"/>
+            <Todolist title="Songs"/>
+            <Todolist title="Boks"/>
+
 
         </div>
     );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Todolist = () => {
+export const Todolist = (props: any) => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <div>
                 <input/>
                 <button>+</button>
