8133eef1dfd8b321387db8fc5714c13a05851c77
msaserj <serg.ks@gmail.com>
2022-05-29 00:44:15 +0300

    01-monday. Array to component todolist and type for array.

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 type PropsType = {
     title: string
+    tasks: Array<TaskType>
 }
+
 
 export const Todolist = (props: PropsType) => {
     return (
@@ -13,9 +20,10 @@ export const Todolist = (props: PropsType) => {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+
             </ul>
             <div>
                 <button>All</button>
