b392949f88d226833b3fb641fbbae98dc837f692
msaserj <serg.ks@gmail.com>
2022-05-29 00:52:03 +0300

    02-monday. Method map() for render tasks

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
     const tasks1 = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "Rest API", isDone: false },
+        { id: 5, title: "GraphQL", isDone: false }
     ]
     const tasks2 = [
         { id: 1, title: "Hello world", isDone: true },
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -20,10 +20,11 @@ export const Todolist = (props: PropsType) => {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
-
+                {props.tasks.map((task) => {
+                    return(
+                            <li key={task.id}><input type="checkbox" checked={task.isDone}/><span>{task.title}</span></li>
+                        )
+                })}
             </ul>
             <div>
                 <button>All</button>
