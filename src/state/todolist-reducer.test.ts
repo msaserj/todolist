0e42f6a8c7ae92b67a06bd155259553a04751c17
msaserj <serg.ks@gmail.com>
2022-05-31 22:15:03 +0300

    08 tuesday. reducer for addTodolist

--- src/state/todolist-reducer.test.ts
+++ src/state/todolist-reducer.test.ts
@@ -18,3 +18,20 @@ test('correct todolist should be removed', () => {
     expect(endState[0].id).toBe(todolistId2)
 })
 
+test('correct todolist should be added', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let newTodolistTitle = 'New Todolist'
+
+    const startState: Array<TodolistsType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    const endState = todolistReducer(startState, {type: 'ADD-TODOLIST', title: newTodolistTitle})
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
+    expect(endState[2].filter).toBe("all")
+})
--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -1,4 +1,5 @@
 import {TodolistsType} from "../App";
+import {v1} from "uuid";
 
 
 type ActionType = {
@@ -10,6 +11,9 @@ export const todolistReducer = (state: Array<TodolistsType>, action: ActionType)
     switch (action.type) {
         case 'REMOVE-TODOLIST':
             return state.filter(tdl => tdl.id !== action.id)
+        case 'ADD-TODOLIST':
+            return [...state, {id: v1(), title: action.title, filter: "all"}]
+
 
         default:
             throw new Error('I don\'t understand this type')
