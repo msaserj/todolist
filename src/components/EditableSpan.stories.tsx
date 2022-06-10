c5cd9c0e5cf94d24e2c2134edcb2feeffe4a638e
msaserj <serg.ks@gmail.com>
2022-06-10 17:54:14 +0300

    12a wednesday. Storybook. EditableSpan story

---
+++ src/components/EditableSpan.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {action} from "@storybook/addon-actions";
+
+import {EditableSpan} from "./EditableSpan";
+
+export default {
+    title: 'EditableSpan Component',
+    component: EditableSpan,
+}
+
+
+const changeCallBack = action("Value changed")
+
+
+
+export const EditableSpanBaseExample = (props: any) => {
+    return <EditableSpan  onChange={changeCallBack} title={"EditableSpan"}/>
+}
