cffe61dea73a6b6dc1d55a2eb148be688103351d
msaserj <serg.ks@gmail.com>
2022-06-21 22:52:22 +0300

    12 storybook AddItemForm.test.js UI-test

---
+++ integration/AddItemForm.test.js
@@ -0,0 +1,14 @@
+
+describe('addItemForm', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        // eslint-disable-next-line no-undef
+        await page.goto('http://localhost:6006/iframe.html?args=&id=additemform-component--add-item-form-base-example&viewMode=story');
+        // eslint-disable-next-line no-undef
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
