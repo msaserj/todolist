3681bbf2b89945a66c2e21c68befe99b7ccb1cb5
msaserj <serg.ks@gmail.com>
2022-07-08 01:45:20 +0300

    13 AXIOS  todolists-api.stories.tsx first test get request

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+
+export default {
+    title: 'API'
+}
+const settings = {
+    withCredentials: true
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        // здесь мы будем делать запрос и ответ закидывать в стейт.
+        // который в виде строки будем отображать в div-ке
+        let promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+        promise.then((res)=>{
+            console.log(res.status)
+            setState(res.data)
+        })
+
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
