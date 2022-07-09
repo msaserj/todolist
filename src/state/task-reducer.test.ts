1910fa82d268a8b9e53400fa5a73ae44ef25c721
msaserj <serg.ks@gmail.com>
2022-07-09 21:25:56 +0300

    13-14 refactoring new types from API for task-reducer.test.ts

--- src/state/task-reducer.test.ts
+++ src/state/task-reducer.test.ts
@@ -1,19 +1,26 @@
 import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, taskReducer} from "./task-reducer";
 import {TasksStateType} from "../AppWithRedux";
 import {addTodolistAC, removeTodolistAC} from "./todolist-reducer";
+import {TaskPriorities, TaskStatuses} from "../api/todolists-api";
 
 let startState: TasksStateType
 beforeEach(()=>{
     startState = {
         'todolistId1': [
-            {id: '1', title: 'CSS', isDone: false},
-            {id: '2', title: 'JS', isDone: true},
-            {id: '3', title: 'React', isDone: false}
+            {id: '1', title: 'CSS', status: TaskStatuses.New, todoListId: "todolistId1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: '2', title: 'JS', status: TaskStatuses.Completed, todoListId: "todolistId1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: '3', title: 'React', status: TaskStatuses.New, todoListId: "todolistId1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
         ],
         'todolistId2': [
-            {id: '1', title: 'bread', isDone: false},
-            {id: '2', title: 'milk', isDone: true},
-            {id: '3', title: 'tea', isDone: false}
+            {id: '1', title: 'bread', status: TaskStatuses.New, todoListId: "todolistId2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: '2', title: 'milk', status: TaskStatuses.Completed, todoListId: "todolistId2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: '3', title: 'tea', status: TaskStatuses.New, todoListId: "todolistId2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
         ]
     }
 })
@@ -25,7 +32,7 @@ test('correct task should be deleted from correct array', () => {
 
     expect(endState["todolistId1"].length).toBe(3)
     expect(endState["todolistId2"].length).toBe(2)
-    expect(endState["todolistId2"].every(t => t.id != "2")).toBeTruthy();
+    expect(endState["todolistId2"].every(t => t.id !== "2")).toBeTruthy();
 })
 
 test('correct task should be added to correct array', () => {
@@ -37,16 +44,16 @@ test('correct task should be added to correct array', () => {
     expect(endState['todolistId2'].length).toBe(4)
     expect(endState['todolistId2'][0].id).toBeDefined()
     expect(endState['todolistId2'][0].title).toBe('juce')
-    expect(endState['todolistId2'][0].isDone).toBe(false)
+    expect(endState['todolistId2'][0].status).toBe(TaskStatuses.Completed)
 })
 
 test('status of specified task should be changed', () => {
 
-    const action = changeTaskStatusAC('2', false, 'todolistId2')
+    const action = changeTaskStatusAC('2', TaskStatuses.New, 'todolistId2')
     const endState = taskReducer(startState, action)
 
-    expect(endState["todolistId2"][1].isDone).toBe(false)
-    expect(endState["todolistId1"][1].isDone).toBe(true)
+    expect(endState["todolistId2"][1].status).toBe(TaskStatuses.New)
+    expect(endState["todolistId1"][1].status).toBe(TaskStatuses.Completed)
 })
 
 test('title of specified task should be changed', () => {
@@ -64,7 +71,7 @@ test('new property with new array should be added when new todolist is added', (
     const endState = taskReducer(startState, action)
 
     const keys = Object.keys(endState)
-    const newKey = keys.find(k => k != 'todolistId1' && k != 'todolistId2')
+    const newKey = keys.find(k => k !== 'todolistId1' && k !== 'todolistId2')
     if (!newKey) {
         throw Error('new key should be added')
     }
