4777f818530804492de90766e4b96a264be17f64
msaserj <serg.ks@gmail.com>
2022-07-09 21:37:56 +0300

    13-14 refactoring new types from API for todolist-reducer.test.ts fix tests

--- src/state/todolist-reducer.test.ts
+++ src/state/todolist-reducer.test.ts
@@ -34,7 +34,7 @@ test('correct todolist should be added', () => {
 
     const endState = todolistReducer(startState, addTodolistAC(newTodolistTitle))
     expect(endState.length).toBe(3)
-    expect(endState[2].title).toBe(newTodolistTitle)
+    expect(endState[0].title).toBe(newTodolistTitle)
     expect(endState[2].filter).toBe("all")
 })
 
