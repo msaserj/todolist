c78435e4866e777ba5205f53fabfb2c3311a042f
msaserj <serg.ks@gmail.com>
2022-07-09 21:27:56 +0300

    13-14 refactoring new types from API for todolists-api.tsx

--- src/api/todolists-api.tsx
+++ src/api/todolists-api.tsx
@@ -23,12 +23,25 @@ type ResponseType<Data = {}> = {
     data: Data
 }
 
+export enum TaskStatuses {
+    New = 0,
+    InProgress = 1,
+    Completed = 2,
+    Draft = 3
+}
+export enum TaskPriorities {
+    Low = 0,
+    Middle = 1,
+    Hi = 2,
+    Urgently = 3,
+    Later = 4
+}
+
 export type TaskType = {
     description: string
     title: string
-    completed: boolean
-    status: number
-    priority: number
+    status: TaskStatuses
+    priority: TaskPriorities
     startDate: string
     deadline: string
     id: string
