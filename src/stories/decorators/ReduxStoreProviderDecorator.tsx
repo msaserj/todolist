3aac9a53df6fc35f472cd2b996f672ea094a894c
msaserj <serg.ks@gmail.com>
2022-07-09 21:25:03 +0300

    13-14 refactoring new types from API for ReduxStoreProviderDecorator.tsx

--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -5,6 +5,7 @@ import {combineReducers} from "redux";
 import {todolistReducer} from "../../state/todolist-reducer";
 import {taskReducer} from "../../state/task-reducer";
 import { legacy_createStore as createStore} from 'redux'
+import {TaskPriorities, TaskStatuses} from "../../api/todolists-api";
 
 const rootRedcer = combineReducers({
     todolists: todolistReducer,
@@ -13,17 +14,21 @@ const rootRedcer = combineReducers({
 
 const initialGlobalState = {
     todolists: [
-        {id: "todolistID1", title: 'What to learn', filter: 'all'},
-        {id: "todolistID2", title: 'What to buy', filter: 'all'},
+        {id: "todolistID1", title: 'What to learn', filter: "all", addedData: '', order: 0},
+        {id: "todolistID2", title: 'What to buy', filter: "all", addedData: '', order: 0},
     ],
     tasks: {
         "todolistID1": [
-            {id: '1', title: 'CSS', isDone: false},
-            {id: '2', title: 'JS', isDone: true},
+            {id: '1', title: 'CSS', status: TaskStatuses.Completed, todoListId: "todolistID1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: '2', title: 'JS', status: TaskStatuses.Completed, todoListId: "todolistID1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
         ],
         "todolistID2": [
-            {id: '1', title: 'bread', isDone: false},
-            {id: '2', title: 'milk', isDone: true},
+            {id: '1', title: 'bread', status: TaskStatuses.Completed, todoListId: "todolistID2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: '2', title: 'milk', status: TaskStatuses.Completed, todoListId: "todolistID2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
         ]
     }
 }
