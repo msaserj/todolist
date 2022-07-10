d4316deac4f2c981b2cf368a61b42a108355ccc5
msaserj <serg.ks@gmail.com>
2022-07-10 23:02:55 +0300

    14 Thunk. add new test task reducer 'empty arrays should be added when we set todolists'

--- src/state/task-reducer.test.ts
+++ src/state/task-reducer.test.ts
@@ -1,6 +1,6 @@
 import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, taskReducer} from "./task-reducer";
 import {TasksStateType} from "../AppWithRedux";
-import {addTodolistAC, removeTodolistAC} from "./todolist-reducer";
+import {addTodolistAC, removeTodolistAC, setTodolistsAC} from "./todolist-reducer";
 import {TaskPriorities, TaskStatuses} from "../api/todolists-api";
 
 let startState: TasksStateType
@@ -87,4 +87,18 @@ test('property with todolistId should be deleted', () => {
 
     expect(keys.length).toBe(1)
     expect(endState['todolistId2']).not.toBeDefined()
+})
+test('empty arrays should be added when we set todolists', () => {
+
+
+    const action = setTodolistsAC([
+        {id: '1', title: 'What to learn', order: 0, addedData: ''},
+        {id: '2', title: 'What to buy', order: 0, addedData: ''}
+    ])
+    const endState = taskReducer({}, action)
+    const keys = Object.keys(endState)
+
+    expect(keys.length).toBe(2)
+    expect(endState['1']).toStrictEqual([])
+    expect(endState['2']).toStrictEqual([])
 })
