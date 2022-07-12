4f5fd0b0f42d054939a7e3d29624e0740b37316e
msaserj <serg.ks@gmail.com>
2022-07-12 16:48:28 +0300

    14 Thunk. New version redux new requirements for more please see https://redux.js.org/usage/usage-with-typescript

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,7 @@
-import {combineReducers, legacy_createStore as createStore} from 'redux'
-import {todolistReducer} from "./todolist-reducer";
-import {taskReducer} from "./task-reducer";
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore as createStore} from 'redux'
+import {TodolistActionsType, todolistReducer} from "./todolist-reducer";
+import {TaskActionsType, taskReducer} from "./task-reducer";
+import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -10,14 +11,21 @@ const rootRedcer = combineReducers({
 })
 
 // определить автоматически тип всего объекта состояния
-export type AppRootState = ReturnType<typeof rootRedcer>
-export const store = createStore(rootRedcer);
+export const store = createStore(rootRedcer, applyMiddleware(thunk));
+
+export type RootState = ReturnType<typeof store.getState>
+//export type AppDispatch = typeof store.dispatch
+export type AppDispatch = ThunkDispatch<RootState, unknown, AppActionsType>
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>
+
+type AppActionsType = TaskActionsType | TodolistActionsType
+
+
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store
-
-
 
 
 // type AppRootState = {
