ef8c7d0b44737da7448faacb320f1cb298e7888c
msaserj <serg.ks@gmail.com>
2022-07-22 02:43:23 +0300

    16 App initializing, routers, redirect, auth/me. Login validate

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,6 +8,9 @@ import {useSelector} from "react-redux";
 import {RootState} from "./store";
 import {RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import {Login} from "../features/Login/Login";
+
 
 type PropsType = {
     demo?: boolean
@@ -16,32 +19,40 @@ type PropsType = {
 function App({demo = false}: PropsType) {
     console.log("App is called")
     const status = useSelector<RootState, RequestStatusType>((state) => state.app.status)
-   return (
-        <div className="App">
-            <ErrorSnackbar/>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{mr: 2}}>
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <ErrorSnackbar/>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{mr: 2}}>
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                            News
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
 
-                {status === 'loading' && <LinearProgress/>}
-            </AppBar>
-            <Container fixed>
-                {/*universal component*/}
-                <TodolistsList demo={demo}/>
-            </Container>
-        </div>
+                    {status === 'loading' && <LinearProgress/>}
+                </AppBar>
+                <Container fixed>
+                    <Routes>
+                        <Route path="/" element={<TodolistsList demo={demo}/>}/>
+                        <Route path="login" element={<Login/>}/>
+                        <Route path="/404" element={<h1>404: PAGE NOT FOUND</h1>}/>
+                        <Route path="*" element={<Navigate to="/404"/>}/>
+                    </Routes>
+                    {/*universal component*/}
+                </Container>
+            </div>
+        </BrowserRouter>
+
 
     );
 }
--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -7,32 +7,83 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {useFormik} from "formik";
 
 export const Login = () => {
+    type FormikErrorType = {
+        email?: string
+        password?: string
+        rememberMe?: boolean
+    }
+
+
+    const formik = useFormik({
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+                errors.password = 'Required';
+            } else if (values.password.length < 3) {
+                errors.password = 'Password must be more 2 symbols';
+            }
+            return errors;
+        },
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2))
+        }
+    })
+
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
-            <FormControl>
-                <FormLabel>
-                    <p>To log in get registered
-                        <a href={'https://social-network.samuraijs.com/'}
-                           target={'_blank'}> here
-                        </a>
-                    </p>
-                    <p>or use common test account credentials:</p>
-                    <p>Email: free@samuraijs.com</p>
-                    <p>Password: free</p>
-                </FormLabel>
-                <FormGroup>
-                    <TextField label="Email" margin="normal"/>
-                    <TextField type="password" label="Password"
-                               margin="normal"
-                    />
-                    <FormControlLabel label={'Remember me'} control={<Checkbox/>}/>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                </FormGroup>
-            </FormControl>
+            <form onSubmit={formik.handleSubmit}>
+                <FormControl>
+                    <FormLabel>
+                        <p>To log in get registered
+                            <a href={'https://social-network.samuraijs.com/'}
+                               target={'_blank'}> here
+                            </a>
+                        </p>
+                        <p>or use common test account credentials:</p>
+                        <p>Email: free@samuraijs.com</p>
+                        <p>Password: free</p>
+                    </FormLabel>
+                    <FormGroup>
+                        <TextField
+                            type="email"
+                            label="Email"
+                            margin="normal"
+                            {...formik.getFieldProps("email")}
+                        />
+                        {formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div> : null}
+                        <TextField
+                            type="password"
+                            label="Password"
+                            margin="normal"
+                            {...formik.getFieldProps("password")}
+                        />
+                        {formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.password}</div> : null}
+                        <FormControlLabel
+                            label={'Remember me'}
+                            control={<Checkbox
+                                {...formik.getFieldProps("rememberMe")}
+                                checked={formik.values.rememberMe}
+                            />}/>
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                </FormControl>
+            </form>
+
         </Grid>
     </Grid>
 }
