226db401f2116278d22bf353e747798caadd7539
msaserj <serg.ks@gmail.com>
2022-09-20 15:49:12 +0300

    18 routes

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -49,7 +49,7 @@ export const Login = () => {
     })
 
     if (isLoggedIn) {
-        return <Navigate to={"/"}/>
+        return <Navigate to={"/it-inc-todolist"}/>
     }
 
     return <Grid container justifyContent={'center'}>
