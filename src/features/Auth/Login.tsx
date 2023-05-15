146fb5694d22c265a6f2114aadf56df98940b8f5
msaserj <serg.ks@gmail.com>
2023-05-15 23:34:42 +0300

    test commit

--- src/features/Auth/Login.tsx
+++ src/features/Auth/Login.tsx
@@ -35,11 +35,7 @@ export const Login = () => {
       if (!values.email) {
         errors.email = "Required";
       }
-      // else if (
-      //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-      // ) {
-      //   errors.email = "Invalid email address";
-      // }
+
       if (!values.password) {
         errors.password = "Required";
       } else if (values.password.length < 3) {
